a4e7e1c80fff113c676807a883f3f29e380dafc0
Tanisha <107809690+Tanishaaaaaaa@users.noreply.github.com>
2025-02-04 19:51:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-<<<<<<< HEAD
 require('dotenv').config(); 
 // MongoDB connection
 //mongoose.connect('mongodb://localhost:27017/devicesDB', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,26 +12,6 @@ require('dotenv').config();
 // Connect to MongoDB Atlas
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/devicesDB'; // For local use
 mongoose.set("strictQuery", false);
-=======
-
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const mongoURI = process.env.MONGO_URI;
-
-mongoose.set("strictQuery", false);
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if DNS fails
-  family: 4 // Force IPv4 for DNS resolution
-})
-  .then(() => console.log("✅ MongoDB Connected to Atlas"))
-  .catch(err => console.error("❌ MongoDB Connection Error:", err));
-
-
->>>>>>> d1d33bd636a85e29535781a0ad6c909fed1d73dc
 
 mongoose.connect(mongoURI, {
   serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if DNS fails
